7516b64939944d294c0d08060aea7be6b8d11be7
Archiraph <raphael.mortiers@hotmail.com>
2023-12-04 16:34:17 +0100

    fin

---
+++ src/page/About.js
@@ -0,0 +1,55 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore
+        dicta rem porro aliquam eligendi laboriosam. Eveniet dolorem numquam
+        minima cum dolorum, voluptas, tempora pariatur nostrum praesentium
+        accusantium alias eos nam architecto repellat ut saepe sequi at ipsam
+        quae autem placeat earum possimus! Minima soluta explicabo ipsum,
+        aliquid libero ab distinctio optio quas iusto atque, repellendus numquam
+        cupiditate aperiam corrupti pariatur, natus et eveniet excepturi? Modi
+        porro, voluptas aspernatur suscipit voluptates ipsum iusto odio nam
+        temporibus, consectetur architecto ex nobis. Aut earum quo incidunt
+        tempore dolor nihil voluptates necessitatibus vel animi. Odit rerum iste
+        maiores eveniet nisi asperiores aliquid ullam numquam?
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate,
+        reiciendis quia explicabo sint accusantium adipisci veritatis quis
+        libero, illum at quibusdam similique doloribus dolore eos error iste
+        repellat voluptatum labore cumque pariatur soluta aliquid. Dolores
+        doloribus maxime reiciendis labore? Itaque nostrum vitae labore
+        consectetur sapiente quam eveniet distinctio debitis dolorem. Voluptatum
+        ipsa laudantium aut ut temporibus quis, error provident illum neque,
+        deleniti excepturi rem soluta possimus debitis. Quis repudiandae,
+        consequuntur nostrum possimus non eligendi numquam at quisquam animi
+        assumenda sed exercitationem? Repudiandae cupiditate corrupti, totam
+        accusantium dicta adipisci error beatae inventore ipsam et excepturi
+        perferendis minus quisquam magnam ab neque dolore eius commodi alias
+        iusto quasi. Et in quidem architecto ipsa perspiciatis accusantium modi,
+        cum eos suscipit repellendus eius! Dolore quae maiores animi. Soluta
+        aspernatur architecto placeat voluptatibus alias, aliquid repellendus
+        atque molestiae recusandae, ab quibusdam itaque corrupti saepe
+        consectetur, eaque autem consequuntur porro temporibus impedit
+        voluptatum nemo tempora rerum minus illum? Neque, quibusdam
+        voluptatibus? Itaque, assumenda? Suscipit, veritatis! Iste cum earum
+        magnam iusto cupiditate animi in, perferendis harum doloribus hic
+        officiis! Vel, sint quasi cum, repellendus excepturi sit adipisci ullam
+        illum totam atque est. Atque totam odit vel, sapiente magni odio
+        molestias sunt sequi! At eaque est libero exercitationem.
+      </p>
+    </div>
+  );
+};
+
+export default About;
